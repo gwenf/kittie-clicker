2b51cec1718d1b1f6acda06086e0260bfe3872f0
Gwendolyn Faraday <gwenfaraday@pm.me>
2021-10-19 09:29:09 -0400

    adding dist for deployment to gh pages

---
+++ kittie-clicker/dist/precache-manifest.18443c08c6bf8a72987e6b642f6b6207.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb35f71895ec58cfef",
+    "url": "/css/app.c255336a.css"
+  },
+  {
+    "revision": "1e3648ceb5bd5fd12cd8",
+    "url": "/css/chunk-vendors.5eec9392.css"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/img/color.c7a33805.png"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "e1d4eab017e41ec00d124e9de6e3f387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5deb35f71895ec58cfef",
+    "url": "/js/app.b6134158.js"
+  },
+  {
+    "revision": "1e3648ceb5bd5fd12cd8",
+    "url": "/js/chunk-vendors.a21a2163.js"
+  },
+  {
+    "revision": "75a6281905bbc76e271c2bff719d87b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
